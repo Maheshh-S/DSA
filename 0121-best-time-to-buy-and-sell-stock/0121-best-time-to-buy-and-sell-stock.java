c8e3d77f6648a001d07415b78d6e1b4bacca0a30
Mahesh <129883017+Maheshh-S@users.noreply.github.com>
2025-02-09 13:53:04 +0530

    Time: 2 ms (75.37%), Space: 62.2 MB (6.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-      int buyprice = Integer.MAX_VALUE;  //from least  min to maximum
-      int  maxprofit = 0; //no profit as of now
-        int n = prices.length;
+        
+        int Buying = prices[0];
+        int profit = 0;
+int n = prices.length;
 
-      for(int i=0;i<n;i++){// 0 to n
-        if(buyprice < prices[i]){ // today i buy should be lesser then the prices array
-            int profit = prices[i] - buyprice; //if yes then i got this
-            maxprofit = Math.max(maxprofit,profit);//check and add to max profit
-        }else{
-            buyprice = prices[i];
+        for(int i=1;i<n;i++){
+            if(prices[i] < Buying){
+                Buying = prices[i];
+            }else if( prices[i] - Buying > profit){
+                profit = prices[i] - Buying;
+            }
         }
-      } 
-      return maxprofit;
+        return profit;
     }
 }
