c4be13cd15bdbd8501bdbf239362db8523828d8d
Mahesh S <129883017+Maheshh-S@users.noreply.github.com>
2026-07-03 13:10:40 +0530

    Time: 0 ms (100%), Space: 48.4 MB (13.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,57 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-          int n = nums.length;
-          int LB = Integer.MAX_VALUE;
-          int UB = -1;
-          for(int i = 0 ; i < n ; i++){
-            if(nums[i] == target){
-                 LB = Math.min(i , LB);
-                 UB = Math.max( i , UB);
-            }
-          }
-          if(LB == Integer.MAX_VALUE) LB = -1;
-          return new int[]{LB , UB};
+        
+        int[] res = new int[2];
+
+        res[0] = First(nums , target);
+        res[1] = Second(nums, target);
+
+        return res;
+
+        
     }
+
+    public int First(int[] nums , int target){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        int ans = -1;
+        while( l <= r){
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                r = mid -1;
+            }
+           else if(nums[mid] <= target){
+                l = mid+1;
+            }else{
+                r = mid -1;
+            }
+        }
+        return ans;
+    }
+
+
+    public int Second(int[] nums , int target){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        int ans2 = -1;
+        while( l <= r){
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] == target){
+                ans2 = mid;
+                l = mid +1;
+            }
+          else  if(nums[mid] <= target){
+                l = mid+1;
+            }else{
+                r = mid -1;
+            }
+        }
+        return ans2;
+    }
+
 }
