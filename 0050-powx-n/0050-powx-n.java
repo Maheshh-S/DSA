80c64934547b8d36d654629d56e4ae9f2d174f06
Mahesh S <129883017+Maheshh-S@users.noreply.github.com>
2026-08-06 23:02:27 +0530

    Time: 0 ms (100%), Space: 48.1 MB (35.12%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,34 @@
 class Solution {
-    public double myPow(double a, int n) {
-        double ans = 1.0;
-        long exp = n; // Convert n to long to handle overflow
+    public double myPow(double x, int n) {
+        
+        long exp = n;
 
-        if (exp < 0) {
-            a = 1 / a;
-            exp = -exp; // Convert negative exponent to positive
+        if(exp < 0){
+            x = 1/x;
+            exp = -exp;
         }
 
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                ans *= a;
-            }
-            a *= a;  // Square the base
-            exp >>= 1;  // Right shift the exponent
-        }
-
-        return ans;
+        return power(x , exp , 1);
     }
+        double power(double x , long n ,double ans){
+            if(n == 0) return ans;
+
+            if(n % 2 != 0){
+                ans *= x;
+            }
+
+            return power(x*x , n/2, ans);
+        }
+        // double ans = 1;
+
+        // while(exp > 0){
+        //     if(exp % 2 != 0){
+        //         ans *= x; //4 , 1024
+        //     }
+
+        //     x *= x; //4 , 16 , 256 , xxxx
+        //     exp /= 2; // 5 , 2 , 1 , 0
+        // } 
+        // return ans;//1024
+    
 }
