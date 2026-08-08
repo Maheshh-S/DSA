87e634d98f6feb739bcf0566f51cd9262bee33f2
Mahesh S <129883017+Maheshh-S@users.noreply.github.com>
2026-08-08 08:48:49 +0530

    Time: 2 ms (47.33%), Space: 48.8 MB (72.28%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,43 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Get the sizes of both input arrays.
+        
         int n = nums1.length;
         int m = nums2.length;
-
-        // Merge the arrays into a single sorted array.
-        int[] merged = new int[n + m];
+        int[] merged = new int[n+m];
         int k = 0;
-        for (int i = 0; i < n; i++) {
-            merged[k++] = nums1[i];
-        }
-        for (int i = 0; i < m; i++) {
-            merged[k++] = nums2[i];
+        int i=0;
+        int j=0;
+        while(i !=n && j!=m){
+            if(nums1[i] <= nums2[j]){
+                merged[k++] = nums1[i++];
+            }else{
+                merged[k++] = nums2[j++];
+            }
         }
 
-        // Sort the merged array.
-        Arrays.sort(merged);
 
-        // Calculate the total number of elements in the merged array.
-        int total = merged.length;
+            while(i != n){
+                merged[k++] = nums1[i++];
+            }
 
-        if (total % 2 == 1) {
-            // If the total number of elements is odd, return the middle element as the median.
-            return (double) merged[total / 2];
-        } else {
-            // If the total number of elements is even, calculate the average of the two middle elements as the median.
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return ((double) middle1 + (double) middle2) / 2.0;
-        }
+            while(j != m){
+                merged[k++] = nums2[j++];
+            }
+
+            int mid = merged.length;
+            double ans = 0.0;
+
+            if(mid % 2 == 0){
+                mid = mid/2;
+                double m1 = merged[mid];
+                double m2 = merged[mid-1];
+                ans = ( m1 + m2 )/2;
+                return ans;
+            }else{
+                mid = mid/2;
+                return merged[mid];
+            }
+
+        
     }
 }
